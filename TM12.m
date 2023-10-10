5b7578ef4eb147c8925b32930a5d524e002d58fd
milarb <125410090+milarb@users.noreply.github.com>
2023-10-10 22:02:21 +1100

    Add files via upload
    Ply files and code for the TM12 robot. Still getting the ply files adjusted so they will work properly with the DH parameters

---
+++ TM12.m
@@ -0,0 +1,49 @@
+classdef TM12 < RobotBaseClass
+    %% TM12 Universal Robot 3kg payload robot model
+    %
+    % WARNING: This model has been created by UTS students in the subject
+    % 41013. No guarentee is made about the accuracy or correctness of the
+    % of the DH parameters of the accompanying ply files. Do not assume
+    % that this matches the real robot!
+
+    properties(Access = public)   
+        plyFileNameStem = 'TM12';
+    end
+    
+    methods
+%% Constructor
+        function self = TM12(baseTr,useTool,toolFilename)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+            end
+          
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr;
+            self.PlotAndColourRobot();
+
+            drawnow
+        end
+
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.1652,'a',0,'alpha',pi/2,'qlim', deg2rad([-270,270]),'offset',0);
+            % link(2) = Link('d',0,'a',0.6361,'alpha',0,'qlim', deg2rad([-180,180]),'offset',0);
+            % link(3) = Link('d',0,'a',0.5579,'alpha',0,'qlim', deg2rad([-166,166]),'offset',0);
+            % link(4) = Link('d',0.106,'a',0,'alpha',-pi/2,'qlim', deg2rad([-180,180]),'offset',0);
+            % link(5) = Link('d',0.106,'a',0,'alpha',pi/2,'qlim', deg2rad([-180,180]),'offset',0);
+            % link(6) = Link('d',0.11315,'a',0,'alpha',0,'qlim', deg2rad([-270,270]),'offset',0);
+             
+            self.model = SerialLink(link,'name',self.name);
+        end      
+    end
+end
